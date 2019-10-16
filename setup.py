ea7d48628d99f5aa8157855e836c6f4ae27d2447
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-10-15 22:48:09 -0400

    modified description

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TG-Tello",
-    version="0.0.2",
+    version="0.0.1",
     author="Danny Dasilva",
     author_email="dannydasilva.solutions@gmail.com",
-    description="Python wrapper for hid device on the pi zero w",
+    description="Tello Python Wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Danny-Dasilva/TG-Tello",
